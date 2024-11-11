ebbf9a7fa7937997b1c255b80553167890805752
Milagros <milimacias01@gmail.com>
2024-11-11 18:39:01 -0300

    Se creo el sidebar de la app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,77 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import {Search, Home, SquareUser, ShoppingBag, ClipboardList} from "lucide-react"
+import {Sidebar, 
+  SidebarProvider,
+  SidebarTrigger, 
+  SidebarHeader,
+  SidebarContent,
+  SidebarGroup, 
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,} from "@/components/ui/sidebar"
+
+  // Menu items.
+  const items = [
+    {
+      title: "Search",
+      url: "search.jsx",
+      icon: Search,
+    },
+    {
+      title: "Principal",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Clientes",
+      url: "#",
+      icon: SquareUser,
+    },
+    {
+      title: "Pedido",
+      url: "#",
+      icon: ShoppingBag,
+    },
+    {
+      title: "Reporte",
+      url: "#",
+      icon: ClipboardList,
+    },
+  ]
 
 function App() {
+
   return (
     <>
+      <SidebarProvider>
+        <Sidebar collapsible="icon">
+          <SidebarTrigger/>
+          <SidebarHeader>
+            <SidebarContent>
+              <SidebarGroup />
+              <SidebarGroupLabel>Tienda Facil</SidebarGroupLabel>
+              <SidebarGroupContent>
+              <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+              </SidebarGroupContent>
+              <SidebarGroup />
+            </SidebarContent>
+          </SidebarHeader>
+        </Sidebar>
+      </SidebarProvider>
       <Button>Hello World</Button>
     </>
   );
