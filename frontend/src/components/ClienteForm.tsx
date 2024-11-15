f4370d8396b2a5e2cb4cc155060328565a67815a
Milagros <milimacias01@gmail.com>
2024-11-15 12:08:49 -0300

    agregando https://zod.dev Validación de esquemas de TypeScript first con inferencia de tipos estáticos

--- frontend/src/components/ClienteForm.tsx
+++ frontend/src/components/ClienteForm.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { z } from "zod" 
 //https://zod.dev/
 import { Button } from "@/components/ui/button"
 import {
@@ -13,20 +13,19 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Checkbox } from "@radix-ui/react-checkbox"
- 
+
 const formSchema = z.object({
   username: z.string().min(2, {
     message: "El nombre debe tener al menos 2 caracteres.",
   }),
   lastname: z.string().min(2, {
-    message: "El nombre de usuario debe tener al menos 2 caracteres.",
+    message: "El apellido debe tener al menos 2 caracteres.",
   }),
-  cel: z.number().min(10, { message: "El celular debe ser al menos 10 caracteres" }),
+  cel: z.string().min(10, { message: "El celular debe tener al menos 10 caracteres" }), // Cambié a z.string() para validarlo como cadena
   email: z.string().email({ message: "El email no es válido" }),
-  estado: z.boolean({ message: "El estado solo puede ser Activo o No Activo"}),
+  estado: z.boolean({ message: "El estado solo puede ser Activo o No Activo" }),
 })
- 
+
 export function ClienteForm() {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -34,12 +33,12 @@ export function ClienteForm() {
     defaultValues: {
       username: "",
       lastname: "",
-      cel: 0,
+      cel: "",
       email: "",
       estado: true,
     },
   })
- 
+
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
@@ -48,77 +47,82 @@ export function ClienteForm() {
   }
 
   return (
-    <><h2>Registro Clientes</h2>
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre</FormLabel>
-              <FormControl>
-                <Input placeholder="Nombre" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-           
-          )} />
-           <FormField
-          control={form.control}
-          name="lastname"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Apellido</FormLabel>
-              <FormControl>
-                <Input placeholder="Apellido" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-           
-          )} />
+    <>
+      <h2>Registro Clientes</h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
-          control={form.control}
-          name="cel"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Celular</FormLabel>
-              <FormControl>
-                <Input placeholder="Celular" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-           
-          )} />
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nombre" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-           
-          )} />
+            control={form.control}
+            name="lastname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Apellido</FormLabel>
+                <FormControl>
+                  <Input placeholder="Apellido" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
-          control={form.control}
-          name="estado"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Estado</FormLabel>
-              <FormControl>
-                <input type="checkbox" name="estado" id="estado" className="items-top flex space-x-2"/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-           
-          )} />
-        <Button type="submit">Registrar</Button>
-      </form>
-    </Form>
+            control={form.control}
+            name="cel"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Celular</FormLabel>
+                <FormControl>
+                  <Input placeholder="Celular" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="estado"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Estado</FormLabel>
+                <FormControl>
+                  <> <input 
+                    type="checkbox"
+                    id="estado"
+                    className="items-top flex space-x-2"
+                  /></>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Registrar</Button>
+        </form>
+      </Form>
     </>
   )
 }
