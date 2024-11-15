ad0c12a681a5b7b407c0a6120ad94d69bb89fa21
jdnarvaez0 <juand.ns07@gmail.com>
2024-11-15 15:18:59 +0000

    agrero clase de configuracion de .env

--- backend/src/main/java/com/easystore/screenmatch/controller/ClienteController.java
+++ backend/src/main/java/com/easystore/screenmatch/controller/ClienteController.java
@@ -1,10 +1,7 @@
 package com.easystore.screenmatch.controller;
 
 import com.easystore.screenmatch.dto.ClienteDTO;
-import com.easystore.screenmatch.model.Cliente;
-import com.easystore.screenmatch.repository.ClienteRepository;
 import com.easystore.screenmatch.service.ClienteService;
-import jakarta.persistence.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/clientes")
